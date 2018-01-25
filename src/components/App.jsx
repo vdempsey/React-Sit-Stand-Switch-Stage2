9f704a6cf6bb9209e012c369fcd94f4fa2823c1a
Valia Dempsey <valentinaml@gmail.com>
2018-01-25 15:24:52 -0800

    Build initial project set-up

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactFontFace from 'react-font-face';
+import '../stylesheets/ui.scss';
+
+
+
+function App() {
+  return(
+    <div className="app">
+    <h1>SIT STAND SWITCH</h1>
+    <p>Lorem Impsum</p>
+    </div>
+  );
+}
+
+
+export default App;
